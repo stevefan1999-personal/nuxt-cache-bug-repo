626413b5953f3fd8fa317873f65d889f29b2675a
Steve Fan <29133953+stevefan1999@users.noreply.github.com>
2018-08-27 19:12:56 +0800

    Redirect to use the default CLI

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-const { Builder, Nuxt } = require('nuxt');
-const config = require('./nuxt.config.js')
-
-const nuxt = new Nuxt(config);
-new Builder(nuxt).build();
-nuxt.listen(3000)
+require("nuxt-edge/bin/nuxt");
